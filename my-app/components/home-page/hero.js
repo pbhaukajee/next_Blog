94d0988ce38ab4fb1ed7e97a9a33d32b511625d0
Priyanka <pbhaukajee@gmail.com>
2024-08-21 10:40:59 -0500

    add hero component

---
+++ my-app/components/home-page/hero.js
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+import classes from "./hero.module.css";
+
+function Hero() {
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/pbhk.png"
+          alt="An image showing Priyanka"
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, I'm Priyanka</h1>
+      <p>
+        I am learning web development - especially frontend frameworks like
+        React.
+      </p>
+    </section>
+  );
+}
+
+export default Hero;
--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,3 +1,12 @@
-function Homepage() {}
+import { Fragment } from "react";
+import Hero from "@/components/home-page/hero";
+
+function Homepage() {
+  return (
+    <Fragment>
+      <Hero />
+    </Fragment>
+  );
+}
 
 export default Homepage;
