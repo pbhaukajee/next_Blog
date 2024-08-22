8094d0b2cca52334f55615cff7e5dcda59b2be35
Priyanka <pbhaukajee@gmail.com>
2024-08-22 11:46:56 -0500

    render code snippet from Markdown

--- my-app/components/posts/post-detail/post-content.js
+++ my-app/components/posts/post-detail/post-content.js
@@ -1,5 +1,7 @@
 import ReactMarkdown from "react-markdown";
 import Image from "next/image";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 import classes from "./post-content.module.css";
 import PostHeader from "./post-header";
@@ -35,6 +37,17 @@ function PostContent(props) {
         );
       }
       return <p>{pararaph.children}</p>;
+    },
+    code(code) {
+      const { className, children } = code;
+      const language = className.split("-")[1]; // className is something like language-js => We need the "js" part here
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={children}
+        />
+      );
     },
   };
 
