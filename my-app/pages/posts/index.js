d1ef4afc91a6570ef75eddef5cc338661dbccb55
Priyanka <pbhaukajee@gmail.com>
2024-08-23 17:09:46 -0500

    add head data

--- my-app/pages/posts/index.js
+++ my-app/pages/posts/index.js
@@ -1,8 +1,21 @@
 import AllPosts from "@/components/posts/all-posts";
 import { getAllPosts } from "@/lib/posts-util";
+import Head from "next/head";
+import { Fragment } from "react";
 
 function AllPostspage(props) {
-  return <AllPosts posts={props.posts} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>All Posts</title>
+        <meta
+          name="description"
+          content="A list of all programming-related posts"
+        />
+      </Head>
+      <AllPosts posts={props.posts} />;
+    </Fragment>
+  );
 }
 
 export function getStaticProps() {
