d691f9fbbc1a0f20ea213bcf244e7d3d0c6d22e9
Priyanka <pbhaukajee@gmail.com>
2024-08-23 16:17:43 -0500

    Store messages in mongodb

---
+++ my-app/pages/api/contact.js
@@ -0,0 +1,51 @@
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const { email, name, message } = req.body;
+
+    if (
+      !email ||
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !message ||
+      message.trim() === ""
+    ) {
+      res.status(422).json({ message: "Invalid Input." });
+      return;
+    }
+
+    const newMessage = { email, name, message };
+
+    let client;
+
+    try {
+      client = await MongoClient.connect(
+        "mongodb+srv://pbhaukajee:lcqOW79pLf7zPXl3@cluster0.evw1t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+      );
+    } catch (error) {
+      res.status(500).json({ message: "Could not connect to database." });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection("messages").insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: "Storing mesage failed!" });
+      return;
+    }
+
+    client.close();
+
+    res
+      .status(201)
+      .json({ message: "Successfully stored message!", message: newMessage });
+  }
+}
+
+export default handler;
